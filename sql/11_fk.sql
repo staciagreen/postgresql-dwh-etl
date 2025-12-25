e6bdfa70976983f450e2999279a2728a8ec4f7e9
staciagreen <staciagreen.1@mail.ru>
2025-12-26 00:41:08 +0300

    fix2

--- sql/11_fk.sql
+++ sql/11_fk.sql
@@ -1,24 +1,19 @@
--- product_category.product_id -> product.product_id
 ALTER TABLE IF EXISTS product_category
   ADD CONSTRAINT fk_pc_product
   FOREIGN KEY (product_id) REFERENCES product(product_id);
 
--- product_category.category_id -> category.category_id
 ALTER TABLE IF EXISTS product_category
   ADD CONSTRAINT fk_pc_category
   FOREIGN KEY (category_id) REFERENCES category(category_id);
 
--- sale.customer_id -> customer.customer_id
 ALTER TABLE IF EXISTS sale
   ADD CONSTRAINT fk_sale_customer
   FOREIGN KEY (customer_id) REFERENCES customer(customer_id);
 
--- sale_item.sale_id -> sale.sale_id
 ALTER TABLE IF EXISTS sale_item
   ADD CONSTRAINT fk_sale_item_sale
   FOREIGN KEY (sale_id) REFERENCES sale(sale_id);
 
--- sale_item.product_id -> product.product_id
 ALTER TABLE IF EXISTS sale_item
   ADD CONSTRAINT fk_sale_item_product
   FOREIGN KEY (product_id) REFERENCES product(product_id);
